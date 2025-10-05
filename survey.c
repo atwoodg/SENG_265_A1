180cbe84fd63068122e551f34060fbc9fe654220
Gabriel Atwood <gabrielatwood@uvic.ca>
2025-10-04 23:03:55 -0700

    added tokenization function and proportions function to build array ofcounts for answers to questions. Also built string struct

--- survey.c
+++ survey.c
@@ -2,8 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINES 50
+#define MAX_CHARS 2000
+
+typedef struct {
+    char array[MAX_LINES][MAX_CHARS];
+    int size;
+} Strings;
+
 //Function to determine the format of the output based on first line giving 0 or 1 combination
-int format(char lines[][500]) {
+int format(char lines[][MAX_CHARS]) {
 
     //Setting variables to tokenize by comma
     int fmt;
@@ -28,6 +36,50 @@ int format(char lines[][500]) {
     }
 }
 
+//Function to tokenize any line into 2D array using specified delimeter
+Strings tokenize(char *line, char delimeter[]) {
+    Strings tokenized;
+    tokenized.size = 0;
+    char* token;
+    int j = 0;
+
+    token = strtok(line, delimeter);
+    while (token != NULL) {
+        while (*token == ' ') token++;
+        strcpy(tokenized.array[tokenized.size], token);
+        token = strtok(NULL, delimeter);
+        tokenized.size++;
+    }
+
+    return tokenized;
+}
+
+void proportions(Strings responses, Strings questions, Strings options, int proportions[questions.size][options.size]) {
+
+    char answers[MAX_LINES][MAX_CHARS];
+    int numAnswers;
+
+    //Filling prop with 0's
+    for (int i = 0; i < questions.size; i++) {
+        for (int j = 0; j < options.size; j++) {
+            proportions[i][j] = 0;
+        }
+    }
+
+    for (int j = 0; j < responses.size; j++) { //Outer loops through lines
+        Strings ops = tokenize(responses.array[j], ",");
+
+        for (int k = 0; k < ops.size; k++) { //Inner loops through answers in line j
+            for (int i = 0; i < options.size; i++) {
+                if (strcmp(ops.array[k], options.array[i]) == 0) {
+                    proportions[k][i] += 1;
+                }
+            }
+        }
+    }
+}
+
+
 int main() {
 
     char buffer[1000];
@@ -37,29 +89,32 @@ int main() {
     //Takes input line by line, checks for comments, and adds to array
     while (fgets(buffer, 1000, stdin) != NULL) {
         if (buffer[0] != '#') {
+            buffer[strcspn(buffer, "\n")] = 0;
             strcpy(lines[i], buffer);
             i += 1;
 
         }
     }
 
-    //Reading in questions/assertions
-    char delim[] = ";";
-    char *token;
-    char questions[12][1000];
-    int j = 0;
+    Strings questions = tokenize(lines[1], ";");
+    Strings options = tokenize(lines[2], ",");
 
-    token = strtok(lines[1], delim);
-    while (token != NULL && j<12) {
-        strcpy(questions[j], token);
-        token = strtok(NULL, delim);
-        j++;
+    Strings responses;
+    responses.size = 0;
+
+    for (int i = 3; i < 13; i++) {
+        strcpy(responses.array[responses.size++], lines[i]);
     }
 
-    for (int j = 0; j < 12; j++) {
-        printf("Question %d: %s\n", j + 1, questions[j]);
-    }
+    int props[questions.size][options.size];
+    proportions(responses, questions, options, props);
 
+    for (int q = 0; q < questions.size; q++) {
+        printf("Q%d: %s\n", q + 1, questions.array[q]);
+        for (int o = 0; o < options.size; o++) {
+            printf("   %s = %d\n", options.array[o], props[q][o]);
+        }
+    }
 
     return 0;
 
